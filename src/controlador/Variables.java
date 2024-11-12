3d268390e02bedadc448f3860692caedd952344c
osvaldo010 <estradaosvaldo150@gmail.com>
2024-11-12 13:15:42 -0600

    Variables estáticas modificadas y encapsuladas.

--- src/controlador/Variables.java
+++ src/controlador/Variables.java
@@ -10,5 +10,72 @@ package controlador;
  */
 public class Variables
 {
-    
+    private static int estadoSemáforoVertical;
+    private static int estadoSemáforoHorizontal;
+    private static int vehiculosEnVertical = 0;
+    private static int vehiculosEnHorizontal = 0;
+
+    /**
+     * @return the estadoSemáforoVertical
+     */
+    public static int getEstadoSemáforoVertical()
+    {
+        return estadoSemáforoVertical;
+    }
+
+    /**
+     * @param aEstadoSemáforoVertical the estadoSemáforoVertical to set
+     */
+    public static void setEstadoSemáforoVertical(int aEstadoSemáforoVertical)
+    {
+        estadoSemáforoVertical = aEstadoSemáforoVertical;
+    }
+
+    /**
+     * @return the estadoSemáforoHorizontal
+     */
+    public static int getEstadoSemáforoHorizontal()
+    {
+        return estadoSemáforoHorizontal;
+    }
+
+    /**
+     * @param aEstadoSemáforoHorizontal the estadoSemáforoHorizontal to set
+     */
+    public static void setEstadoSemáforoHorizontal(int aEstadoSemáforoHorizontal)
+    {
+        estadoSemáforoHorizontal = aEstadoSemáforoHorizontal;
+    }
+
+    /**
+     * @return the vehiculosEnVertical
+     */
+    public static int getVehiculosEnVertical()
+    {
+        return vehiculosEnVertical;
+    }
+
+    /**
+     * @param aVehiculosEnVertical the vehiculosEnVertical to set
+     */
+    public static void setVehiculosEnVertical(int aVehiculosEnVertical)
+    {
+        vehiculosEnVertical = aVehiculosEnVertical;
+    }
+
+    /**
+     * @return the vehiculosEnHorizontal
+     */
+    public static int getVehiculosEnHorizontal()
+    {
+        return vehiculosEnHorizontal;
+    }
+
+    /**
+     * @param aVehiculosEnHorizontal the vehiculosEnHorizontal to set
+     */
+    public static void setVehiculosEnHorizontal(int aVehiculosEnHorizontal)
+    {
+        vehiculosEnHorizontal = aVehiculosEnHorizontal;
+    }
 }
