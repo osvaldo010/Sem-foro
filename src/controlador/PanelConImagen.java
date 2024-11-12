4157edc3a9d2b1d0110b6f3b7fe624c14b0d415b
osvaldo010 <estradaosvaldo150@gmail.com>
2024-11-12 13:18:21 -0600

    Creación de clase exclusiva para un panel personalizado.

---
+++ src/controlador/PanelConImagen.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelConImagen extends JPanel
+{
+
+    private Image imagenFondo;
+
+    public PanelConImagen(String rutaImagen)
+    {
+        this.imagenFondo = new ImageIcon(getClass().getResource(rutaImagen)).getImage();
+        this.setPreferredSize(new Dimension(imagenFondo.getWidth(null), imagenFondo.getHeight(null)));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        g.drawImage(imagenFondo, 0, 0, getWidth(), getHeight(), this); 
+    }
+}
